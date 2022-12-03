364e5efaa8f12514505b5d768289aeae51037ad8
Pedro F. Costa <pedro.filipe@talkdesk.com>
2022-12-03 16:02:34 +0000

    chore: day-3 part 2

---
+++ src/day-3.ts
@@ -0,0 +1,51 @@
+import { readInput, toChunks } from "./utils";
+
+const getCompartments = (rucksack: string) => {
+  const half = Math.round(rucksack.length / 2);
+  return [rucksack.slice(0, half), rucksack.slice(half)];
+};
+
+const getCommonItem = (...compartments: string[]) => {
+  let chars: any = {};
+  const [first, ...others] = compartments.sort((a, b) => b.length - a.length);
+
+  return Array.from(first)
+    .map((c) => {
+      chars[c] = c;
+      return c;
+    })
+    .find((c) => others.every((o) => o.includes(c)));
+};
+
+const getPriority = (item: string | undefined) => {
+  if (!item) {
+    // Should never happen
+    throw new Error("Invalid item");
+  }
+
+  const ascii = item.charCodeAt(0);
+  const delta = ascii >= 97 ? 96 : 38;
+
+  return ascii - delta;
+};
+
+const input = readInput(3);
+
+// Challenge 1
+const total = input.reduce((acc, rucksack) => {
+  const [compartment1, compartment2] = getCompartments(rucksack);
+  const commonItem = getCommonItem(compartment1, compartment2);
+  const priority = getPriority(commonItem);
+
+  return acc + priority;
+}, 0);
+
+// Challenge 2
+const total2 = toChunks(input, 3).reduce((acc, chunk) => {
+  const commonItem = getCommonItem(...chunk);
+  const priority = getPriority(commonItem);
+
+  return acc + priority;
+}, 0);
+
+console.log(total, total2);
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,14 @@
 import fs from "fs";
 import path from "path";
 
+export const toChunks = <T>(array: Array<T>, size: number) =>
+  array.reduce((acc, _, i) => {
+    if (i % size === 0) {
+      acc.push(array.slice(i, i + size));
+    }
+    return acc;
+  }, [] as Array<Array<T>>);
+
 export const readInput = (day: number) =>
   fs
     .readFileSync(path.resolve(__dirname, `../inputs/input-${day}.txt`))
